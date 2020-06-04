e60777cf8e8d013cdb87adb2d70c0de79bb53dfc
techOxio <teamOxio@123>
2020-06-04 16:15:47 +0530

    version 0.5.3.1

--- web/index.php
+++ web/index.php
@@ -1,6 +1,5 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $dotenv=new Symfony\Component\Dotenv\Dotenv();
 $dotenv->load(__DIR__."/../.env");
@@ -9,6 +8,7 @@ $dotenv->load(__DIR__."/../.env");
 defined('YII_DEBUG') or define('YII_DEBUG', ($_ENV['YII_DEBUG']==="true" ? true : false));
 defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
 
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
